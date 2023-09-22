a886d83a41c141bcd4fbb0fbbbd6f4ea7c7e1392
nakulsudhakar035 <51785516+nakulsudhakar035@users.noreply.github.com>
2023-09-22 22:03:39 +0530

    Initial commit

---
+++ src/main/java/com/example/buildingapi/BuildingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingApiApplication.class, args);
+    }
+
+}
